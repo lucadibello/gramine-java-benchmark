5b9ddba99ab0a45bc27e48a7746b277d3e20f76d
Luca Di Bello <lucadibello@proton.me>
2025-10-28 17:34:11 +0000

    now moved to plain  artifact ID + created working example

--- teaclave-bench/src/common/java/com/benchmark/teaclave/bench/common/Service.java
+++
@@ -1,8 +0,0 @@
-package com.benchmark.teaclave.common;
-
-import org.apache.teaclave.javasdk.common.annotations.EnclaveService;
-
-@EnclaveService
-public interface Service {
-    String sayHelloWorld();
-}
